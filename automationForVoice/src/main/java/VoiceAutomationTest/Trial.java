fed395480e1697aea3fdef8c72cbcebefc132178
KITSBaidantiBiswas <baidanti.biswas@tcs.com>
2018-07-23 21:02:20 +0530

    [Update Trial to accommodate Get scenario]

--- automationForVoice/src/main/java/VoiceAutomationTest/Trial.java
+++ automationForVoice/src/main/java/VoiceAutomationTest/Trial.java
@@ -5,47 +5,68 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.tcs.automationForVoice.model.Order;
+
 
 public class Trial {
 
-	public WebDriver initDriver() {
+	public WebDriver initDriver()
+	{
 		WebDriver driver;
-		System.setProperty("webdriver.chrome.driver", "G:/chromedriver.exe");
-		driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "D:/Chrome/chromedriver.exe");
+		driver=new ChromeDriver();
 		return driver;
 	}
 
-	public Order RetrieveOrderDetail(String OrderNo) {
-		WebDriver driver = initDriver();
-		String queryResult = null;
-		String submitDate = null;
-		String creationDate = null;
-		String lastModifiedDate = null;
-		String orderState = null;
+	public Order RetrieveOrderDetail(String OrderNo)
+	{
+		WebDriver driver=initDriver();
+		String queryResult=null;
+		String submitDate=null;
+		String creationDate=null;
+		String lastModifiedDate=null;
+		String orderState=null;
+		String orderOriginSource=null;
+		String totalToPay=null;
+		String paymentGroups=null;
+		String jurisdiction=null;
+		String stateDetail=null;
 
 		driver.get(Origin.GET_USER_DYN_URL.getValue());
 
-		String initialOrderQuery = "<query-items item-descriptor=" + '"' + "order" + '"' + ">sapOrderId=" + '"' + OrderNo + '"' + "</query-items>";
+		String initialOrderQuery="<query-items item-descriptor="+'"'+"order"+'"'+">sapOrderId="+'"'+OrderNo+'"'+"</query-items>";
 
-		Order od = new Order();
+		Order od=new Order();
+
 
 		((JavascriptExecutor) driver).executeScript("arguments[0].value = arguments[1];", driver.findElement(By.xpath(Origin.DYNADMIN_PAGE_COMPONENT_BROWSER_QUERY_TEXTBOX.getValue())), initialOrderQuery);
 		driver.findElement(By.xpath(Origin.DYNADMIN_PAGE_COMPONENT_BROWSER_QUERY_ENTER_BUTTON.getValue())).click();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		queryResult = driver.findElement(By.xpath(Origin.DYNADMIN_PAGE_COMPONENT_BROWSER_QUERY_RESULT.getValue())).getText();
-		submitDate = fetchValue(queryResult, "submittedDate");
-		creationDate = fetchValue(queryResult, "creationDate");
-		lastModifiedDate = fetchValue(queryResult, "lastModifiedDate");
-		orderState = fetchValue(queryResult, "state");
-		orderState = Origin.valueOf(orderState.toUpperCase()).getValue();
+		submitDate = fetchValue(queryResult,"submittedDate");
+		creationDate=fetchValue(queryResult,"creationDate");
+		lastModifiedDate=fetchValue(queryResult,"lastModifiedDate");
+		orderState=fetchValue(queryResult,"state");
+		orderState=Origin.valueOf(orderState.toUpperCase()).getValue();
+		orderOriginSource=fetchValue(queryResult,"orderOriginSource");
+		totalToPay=fetchValue(queryResult,"totalToPay");
+		paymentGroups=fetchValue(queryResult,"paymentGroups");
+		jurisdiction=fetchValue(queryResult,"jurisdiction");
+		stateDetail=fetchValue(queryResult,"stateDetail");
+
+
 
 		od.setSubmitDate(submitDate);
 		od.setCreationDate(creationDate);
 		od.setLastModifiedDate(lastModifiedDate);
 		od.setOrderState(orderState);
+		od.setJurisdiction(jurisdiction);
+		od.setOrderOriginSource(orderOriginSource);
+		od.setPaymentGroups(paymentGroups);
+		od.setStateDetail(stateDetail);
+		od.setTotalToPay(totalToPay);
 
 		driver.quit();
 
@@ -53,22 +74,24 @@ public class Trial {
 
 	}
 
-	public String fetchValue(String queryResult, String value) {
+	public String fetchValue(String queryResult,String value) {
 		String result = null;
 		if (!queryResult.contains("No items returned")) {
-			if (queryResult.contains(value)) {
-				result = queryResult.split(value + '"' + "><!" + "\\[" + "CDATA" + "\\[")[1].split("]")[0].trim();
+			if(queryResult.contains(value))
+			{
+				result = queryResult.split(value + '"' + "><!" + "\\["
+						+ "CDATA" + "\\[")[1].split("]")[0].trim();
 			}
+
 
 		}
 		return result;
 
 	}
 
-//	public static void main(String args[]) {
-//		Trial t = new Trial();
-//		Order o = t.RetrieveOrderDetail("1000016295");
-//		System.out.println(o.getCreationDate());
-//	}
+
+
+
+
 
 }
