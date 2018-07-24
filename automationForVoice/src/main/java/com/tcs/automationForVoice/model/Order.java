f6ab9f43973c0852283216474109e3de6b967a01
KITSBaidantiBiswas <baidanti.biswas@tcs.com>
2018-07-24 12:24:15 +0530

    Updating the order.java

--- automationForVoice/src/main/java/com/tcs/automationForVoice/model/Order.java
+++ automationForVoice/src/main/java/com/tcs/automationForVoice/model/Order.java
@@ -1,4 +1,4 @@
-package com.tcs.automationForVoice.model;
+package VoiceAutomationTest;
 
 public class Order {
 	
@@ -6,6 +6,12 @@ public class Order {
 	private String creationDate;
 	private String lastModifiedDate;
 	private String orderState;
+	private String orderOriginSource;
+	private String totalToPay;
+	private String paymentGroups;
+	private String jurisdiction;
+	private String stateDetail;
+	
 	
 	
 	public String getSubmitDate() {
@@ -31,6 +37,36 @@ public class Order {
 	}
 	public void setOrderState(String orderState) {
 		this.orderState = orderState;
+	}
+	public String getOrderOriginSource() {
+		return orderOriginSource;
+	}
+	public void setOrderOriginSource(String orderOriginSource) {
+		this.orderOriginSource = orderOriginSource;
+	}
+	public String getTotalToPay() {
+		return totalToPay;
+	}
+	public void setTotalToPay(String totalToPay) {
+		this.totalToPay = totalToPay;
+	}
+	public String getPaymentGroups() {
+		return paymentGroups;
+	}
+	public void setPaymentGroups(String paymentGroups) {
+		this.paymentGroups = paymentGroups;
+	}
+	public String getJurisdiction() {
+		return jurisdiction;
+	}
+	public void setJurisdiction(String jurisdiction) {
+		this.jurisdiction = jurisdiction;
+	}
+	public String getStateDetail() {
+		return stateDetail;
+	}
+	public void setStateDetail(String stateDetail) {
+		this.stateDetail = stateDetail;
 	}
 
 	
